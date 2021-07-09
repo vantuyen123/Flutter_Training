bb8aa3b5766d07e4ec560cb730b9ba8de4c27d35
tuyendvCodluck <tuyendv@codluck.com>
2021-07-09 08:50:46 +0700

    create directory structure

---
+++ Flutter UI/movie_app/lib/model/person_response.dart
@@ -0,0 +1,17 @@
+import 'package:movie_app/model/person.dart';
+
+class PersonResponse {
+  final List<Person> persons;
+  final String error;
+
+  PersonResponse(this.persons, this.error);
+
+  PersonResponse.fromJson(Map<String, dynamic> json)
+      : persons =
+            (json["results"] as List).map((e) => Person.fromJson(e)).toList(),
+        error = "";
+
+  PersonResponse.withError(String errorValue)
+      : persons = [],
+        error = errorValue;
+}
