44c09cc28a1b67f80ca38167842195ba419fcc14
tuyendvCodluck <tuyendv@codluck.com>
2021-09-08 00:51:56 +0700

    create model and repository

---
+++ Application handle logic/i_game_database/lib/model/game_models/cover.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+class CoverModel{
+  final int id;
+  final int height;
+  final int width;
+  final String imageId;
+  final String url;
+
+  CoverModel(this.id, this.height, this.width, this.imageId, this.url);
+
+  CoverModel.fromJson(Map<String,dynamic> json)
+  : id = json["id"],
+  height = json["height"],
+  width = json["width"],
+  imageId = json["image_id"],
+  url = json["url"];
+
+}
